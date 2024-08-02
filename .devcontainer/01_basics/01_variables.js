73670d00c5d29e101c32310909f04d1fac811853
ManuThala <manuthala7@gmail.com>
2024-08-02 18:27:23 +0000

    variables and datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=143143;
+let actEmail="manulsmanu7@gmail.com";
+var accountPassword="Dhoni@123";
+accountCity="Tumkur";
+let accountState;
+
+
+// accountId=234;  not allowed to assignment to const variable
+
+actEmail='harsha@gmail.com';
+accountPassword='Kohli@123';
+accountCity='Bangllore';
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,actEmail,accountPassword,accountCity,accountState]);
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; //treat all js code as new version
+
+// alert('hello');  we are using nodejs not browser
+
+
+console.log(3
+    +
+    3); //this is not good practice
+console.log('manu');
+
+let name='manu';
+let age=26;
+let isLoggedIn=true;
+let state=null;
+let secondName;
+
+
+
+// number=>2^53
+//bigint
+//string=>""
+//boolean=>true,false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+
+//object
+console.log(typeof "manu");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+ 
+
+
+
+
+
