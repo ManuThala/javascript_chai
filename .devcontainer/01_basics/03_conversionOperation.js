a26f2ee42c5fb16c6d953fe78d3e39d7b5eae06d
ManuThala <manuthala7@gmail.com>
2024-08-03 06:32:31 +0000

    some extra operations

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,33 +1,73 @@
-let score=null
+// let score=null
 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
+// let valueInNumber=Number(score)
 
-console.log(typeof(valueInNumber),",",valueInNumber);
+// console.log(typeof(valueInNumber),",",valueInNumber);
 
 
 // "33" => 33
 // "33abc"=>NAN
 // true =>1; false=>0
 
-let isLoggedIn=1;
+// let isLoggedIn=1;
 
-let booleanILoggedIn=Boolean(isLoggedIn)
+// let booleanILoggedIn=Boolean(isLoggedIn)
 
-console.log(booleanILoggedIn);
+// console.log(booleanILoggedIn);
 
 //1 =>true;0=>false
 //""=>false
 //"manu"=true
 
 
-let someNumber=33
+// let someNumber=33
 
-let stringNumber=String(someNumber)
+// let stringNumber=String(someNumber)
 
-console.log(stringNumber,",",typeof(stringNumber));
+// console.log(stringNumber,",",typeof(stringNumber));
+
+//******************operations ******************/
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1='hello'
+// let str2='world'
+// let str3=str1+str2
+// console.log(str3);
+
+
+// console.log("1"+1);
+// console.log(1+"1");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true); //1
+// console.log(+""); //0
+// console.log(true); //true
+
+// let num1=num2=num3
+// num1=num2=num3=2+2;
+
+let gameCounter=100
+++gameCounter;
+gameCounter++;
+
+console.log(gameCounter);
+
 
 
