6b7f5f7e215a55d0158698620848432d9af276b0
ManuThala <manuthala7@gmail.com>
2024-08-05 03:00:12 +0000

    string extra operations

--- .devcontainer/01_basics/05_strings.js
+++ .devcontainer/01_basics/05_strings.js
@@ -3,17 +3,35 @@ const repoCount=50
 
 // console.log((name + repoCount + " value"));
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 
-const gameName=new String('manu');
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+const gameName=new String('manu-ls-harsh');
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('n'));
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newString=gameName.substring(0,4);
+console.log(newString);
+
+const anotherString=gameName.slice(-8,3)
+console.log(anotherString);
+
+const newString1="   manoj ";
+console.log(newString1);
+console.log(newString1.trim());
+
+const url='https://manu.com/%20choudry';
+console.log(url.replace('%20','-'));
+
+
+console.log( url.includes('manu'));
+
+console.log(gameName.split('-'));
 
 
