14125dc5f2d25a4f9ceeb0e44729765732cf12e9
ManuThala <manuthala7@gmail.com>
2024-08-05 03:23:32 +0000

    maths and numbers

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score=400;
+// console.log(score);
+
+const number=new Number(score);
+// console.log(number);
+
+// console.log(number.toString().length);
+// console.log(number.toFixed(2));
+
+const otherNumber=23.9555
+
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds=100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++ Mathhs +++++++++++
+
+// console.log(Math.abs(-4)); //convert negative to positive
+// console.log(Math.round(4.3)); // 4  near by number
+// console.log(Math.ceil(4.3));  // 5 ceiling value
+// console.log(Math.floor(4.6));  // 4 floor value
+// console.log(Math.sqrt(25));  // 5 square root of 25 
+
+
+console.log(Math.random());
+console.log((Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
