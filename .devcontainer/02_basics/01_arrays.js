bf0c566436fcaa47a8a1270191b06ade405aff2c
ManuThala <manuthala7@gmail.com>
2024-08-06 02:41:34 +0000

    extended array concepts

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -27,16 +27,15 @@ const myArr2=[1,2,3,4,5]
 
 //slice spice
 
-console.log("A ",myArr);
-const myn1=myArr.slice(1,3);
-console.log(myn1);
-console.log("B ",myArr);
+// console.log("A ",myArr);
+// const myn1=myArr.slice(1,3);
+// console.log(myn1);
+// console.log("B ",myArr);
 
-const myn2=myArr.splice(1,3);
-console.log(myArr);
+// const myn2=myArr.splice(1,3);
+// console.log(myArr);
 
-console.log(myn2);
-
+// console.log(myn2);
 
 
 
