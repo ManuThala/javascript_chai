d28744ac77247cad8ee67a5501a0db08aed54c59
ManuThala <manuthala7@gmail.com>
2024-08-09 03:26:39 +0000

    extended object part

--- .devcontainer/02_basics/04_objects2.js
+++ .devcontainer/02_basics/04_objects2.js
@@ -1,51 +1,67 @@
-// const tinderUser=new Object();
+// // const tinderUser=new Object();
 
-const tinderUser={}
-tinderUser.id='123abc'
-tinderUser.name='manu'
-tinderUser.isLoggedIn=false
+// const tinderUser={}
+// tinderUser.id='123abc'
+// tinderUser.name='manu'
+// tinderUser.isLoggedIn=false
 
+// // console.log(tinderUser);
+
+// const regularUser={
+//     email:'some@gmail.com',
+//     fullname:{
+//         userfullnmae:{
+//             FirstName:'manu',
+//             lastName:'ls'
+
+//         }
+//     }
+// }
+// console.log(regularUser.fullname.userfullnmae.FirstName);
+
+// const obj1={1:'a',2:'b'}
+// const obj2={3:'c',4:'d'}
+// const obj4={5:'e',6:'f'}
+
+// // const obj3={obj1,obj2} { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+// // const obj3=Object.assign({},obj1,obj2,obj4)
+
+// const obj3={...obj1,...obj2,...obj4}
+// // console.log(obj3);
+
+
+// const users=[
+//     {
+//         id:1,
+//         email:'mannu@gmail.com'
+//     },
+//     {
+//         id:2,
+//         email:'har@gmail.com'
+//     }
+// ]
+
+// users[1].email
 // console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-const regularUser={
-    email:'some@gmail.com',
-    fullname:{
-        userfullnmae:{
-            FirstName:'manu',
-            lastName:'ls'
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
-        }
-    }
+
+const course={
+    courseName:"js in kannada",
+    price:'999',
+    courseInstructor:"manu"
 }
-console.log(regularUser.fullname.userfullnmae.FirstName);
 
-const obj1={1:'a',2:'b'}
-const obj2={3:'c',4:'d'}
-const obj4={5:'e',6:'f'}
-
-// const obj3={obj1,obj2} { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
-
-// const obj3=Object.assign({},obj1,obj2,obj4)
-
-const obj3={...obj1,...obj2,...obj4}
-// console.log(obj3);
+const {courseInstructor:instructor}=course
+console.log(instructor);
 
 
-const users=[
-    {
-        id:1,
-        email:'mannu@gmail.com'
-    },
-    {
-        id:2,
-        email:'har@gmail.com'
-    }
-]
+const navbar=({companyName})=>{
 
-users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+}
+navbar(companyName='manu')
