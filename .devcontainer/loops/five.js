b25945a22746effce86e24d1a008af18ccf0806f
ManuThala <manuthala7@gmail.com>
2024-08-10 19:00:54 +0000

    FOR EACH USING OBJECTS

---
+++ .devcontainer/loops/five.js
@@ -0,0 +1,45 @@
+const coading=['js','ruby','python','cpp','java']
+
+// coading.forEach(element => {
+//     console.log(element);
+    
+// });
+
+// coading.forEach(function (value){
+//     console.log(value);
+    
+// })
+
+
+// coading.forEach((i)=>{
+//     console.log(i);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+//     }
+// coading.forEach(printMe)
+
+// coading.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);   
+// })
+
+const myCoading=[{
+    lang:'javascript',
+    languageFile:'js'
+},
+{
+    lang:'java',
+    languageFile:'java'
+},
+{
+    lang:'python',
+    languageFile:'py'
+},
+]
+
+myCoading.forEach((item)=>{
+    console.log(item.lang);
+    
+})
