22f16cfd05d88a7943940c3963fe3fb46b9969ea
ManuThala <manuthala7@gmail.com>
2024-08-10 13:55:57 +0000

    if else conditions

---
+++ .devcontainer/04_basics/one.js
@@ -0,0 +1,71 @@
+// const isUserLoggedIn=true;
+// if(isUserLoggedIn){
+//     console.log('user logged in succesfully');
+    
+// }
+
+// if(true){
+//     name='manu'
+//     console.log(`hii ${name}`);
+// }
+
+
+// if(2=='2')
+//     console.log('equal')
+
+// else
+//     console.log('not equal');
+
+
+
+// if(2==='2')
+//     console.log('equal')
+
+// else
+//     console.log('not equal');
+
+
+
+// const score=400
+// if(score>100){
+//     let power='fly'
+//     console.log(`user power:${power}`); 
+// }
+// console.log(`user power:${power}`);
+
+
+//short hand notation
+
+const balance=1000
+
+// if(balance>500) console.log('test'),
+// console.log('teest22');
+
+// if(balance < 500 ){
+//     console.log('less than');
+    
+// }else if(balance < 750){
+//     console.log('less than 750');
+    
+// }else if(balance < 900){
+//     console.log('less than 900');
+    
+// }else{
+//     console.log('less than 1200');
+    
+// }
+
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedFromEmail=true
+
+if(userLoggedIn && debitCard){
+    console.log('allowed to buy course');   
+}
+if(loggedFromEmail || loggedInFromGoogle){
+    console.log('user logged in');    
+}
+
+
