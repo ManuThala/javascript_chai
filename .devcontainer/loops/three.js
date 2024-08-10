b5c3b4fc5cfd01acee811888a1a64461d81bc4be
ManuThala <manuthala7@gmail.com>
2024-08-10 19:01:27 +0000

    MAPS IN JAVASCRIPT

---
+++ .devcontainer/loops/three.js
@@ -0,0 +1,39 @@
+//for of loop
+
+//['','','']
+//[{},{},{}]
+
+// const arr=[1,2,3,4,5,6]
+// for (const i of arr) {
+//     console.log(`array value is : ${i}`);  
+// }
+
+// const greetings='hello world'
+// for (const i of greetings) {
+//     console.log(i);
+    
+// }
+
+
+//maps
+
+const map=new Map()
+map.set('IN','India')
+map.set('AUS','Australia')
+map.set('US','America')
+map.set('UK','England')
+
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,':-',value);
+    
+// }
+
+const mmyObject={
+    'game1':'nfs',
+    'game2':'spiderman'
+}
+
+// for (const [key,value] of mmyObject) {
+//          console.log(key,':-',value);
