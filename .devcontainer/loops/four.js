47ba8ea5827628036ca3f0d0a4cc6a3366de9aa2
ManuThala <manuthala7@gmail.com>
2024-08-10 19:01:12 +0000

    FORIN LOOP FOR OBJECTS

---
+++ .devcontainer/loops/four.js
@@ -0,0 +1,26 @@
+const myObject={
+    js:'javascript',
+    cpp:'c++',
+    py:'python'
+}
+
+for (const key in myObject) {
+    console.log(myObject[key]);
+}
+
+const programming=['js','java','py','cpp']
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// const map=new Map()
+// map.set('IN','India')
+// map.set('AUS','Australia')
+// map.set('US','America')
+// map.set('UK','England')
+
+// for (const key in map) {
+//     console.log(map[key]);    
+// }
+
+
