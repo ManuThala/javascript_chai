5b39647560df190085ebed3ba10641069fd897c2
ManuThala <manuthala7@gmail.com>
2024-08-10 13:56:23 +0000

    switch statement syntax and explanation

---
+++ .devcontainer/04_basics/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month='Jan'
+
+switch (month) {
+    case 'Jan':
+        console.log('January');
+        break;
+    case 'Feb':
+        console.log('February');
+        break;
+    case 'Mar':
+        console.log('March');
+        break;
+    case 'Apr':
+        console.log('April');
+        break;
+
+    default:
+        console.log('enter correct month');
+        break;
+}
